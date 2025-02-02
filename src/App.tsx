d5226bd4263b8963147157557f9d24f06c7e3405
Divyam Singh <divyamsingh1918@gmail.com>
2025-02-02 16:45:09 +0530

    intital commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+ 
+import './App.css'
+import {Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import AboutUs from './pages/AboutUs'
+import Contact from './pages/Contact'
+import AddPost from './pages/AddPost'
+import Allposts from './pages/Allposts'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+function App() {
+   
+
+  return (
+    <div>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/about' element={<AboutUs/>}></Route>
+          <Route path='/contact' element={<Contact/>}></Route>
+          <Route path='/login' element={<Login/>}></Route>
+          <Route path='/signup' element={<Signup/>}></Route>
+          <Route path='/add-post' element={<AddPost/>}></Route>
+          <Route path='/all-posts' element={<Allposts/>}></Route>
+        </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/SpalshScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SpalshScreen = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SpalshScreen
