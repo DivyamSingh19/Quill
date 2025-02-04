ba20e2839209628f8a11e105303446d6d951b6ee
Divyam Singh <divyamsingh1918@gmail.com>
2025-02-04 17:27:31 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import {BrowserRouter} from 'react-router-dom' 
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import store from './store/store.ts'
 import App from './App.tsx'
+import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
+  <Provider store={store}>
   <App />
+  </Provider>
  </BrowserRouter>
 )
