3f2bb4b25752f647af7e6feb4ed94213559fdc45
Divyam Singh <divyamsingh1918@gmail.com>
2025-02-04 23:41:06 +0530

    Update Allposts.tsx

--- src/pages/Allposts.tsx
+++ src/pages/Allposts.tsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import { useEffect , useState } from "react"
+import appwriteService from "../appwrite/config"
+function Allposts()  {
+  const [posts,setPosts] = useState([]);
+  const [searchQuery , setSearchQuery] = useState('');
+  useEffect(()=>{
+     appwriteService.getPosts([]).then((posts)=>{
+      if(posts){
+        setPosts(posts.documents);
+      }
+     })
+  },[])
 
-const Allposts = () => {
+
+  const filteredPosts = posts.filter((posts)=>
+   posts.title.toLowerCase().includes(searchQuery.toLowerCase())
+  
+  );
+  const breakpointColumnsObj ={
+    default :4 ,
+    1280:4,
+    1024:3,
+    768:2,
+    640:1
+  };
   return (
     <div>
       
