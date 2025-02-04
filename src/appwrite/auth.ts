b1349cc0884045b44b9862fe8c045fd06b57a2a5
Divyam Singh <divyamsingh1918@gmail.com>
2025-02-04 11:30:53 +0530

    Update auth.ts

--- src/appwrite/auth.ts
+++ src/appwrite/auth.ts
@@ -1,100 +1,82 @@
 import conf from "../conf/conf";
-import {Client,Account ,ID ,Databases} from "appwrite"
-import {toast} from 'react-toastify'
+import { Client, Account, ID } from "appwrite";
+import { toast } from "react-toastify";
 
-interface User{
-    email:string,
-    password : string,
-    name:string,
-    
+interface User {
+    email: string;
+    password: string;
+    name?: string;
 }
 
-export class AuthService{
-    client = new Client();
-    account;
+class AuthService {
+    private client: Client;
+    private account: Account;
 
-    constructor(){
-        this.client
+    constructor() {
+        this.client = new Client()
             .setEndpoint(conf.appwriteUrl)
             .setProject(conf.appwriteProjectId);
         this.account = new Account(this.client);
     }
-    async createAccount({email,password,name}:User){
+
+    async createAccount({ email, password, name }: User) {
         try {
-            const userAccount = await this.account.create(ID.unique(),email,password,name)
-            if(userAccount){
-                return await this.login({email,password})
-                 
-            }
-            else{
-                return userAccount;
-            }
-            toast.success("Account Created Successfully") 
+            const userAccount = await this.account.create(
+                ID.unique(),
+                email,
+                password,
+                name
+            );
             
+            if (userAccount) {
+                toast.success("Account Created Successfully");
+                return await this.login({ email, password });
+            }
+            return userAccount;
         } catch (error) {
-        
-            console.log(error);
-            toast.error('User could not be created')
+            console.log("Create account error:", error);
+            toast.error("User could not be created");
+            throw error;
         }
-    }
-    async login({email,password}:User){
-        try {
-            toast.success("Login successful")
-            return await this.account.createEmailPasswordSession(email,password);
-        } catch (error) {
-            console.log(error);
-            toast.error("Couldn't login now .Try Again Later")
-        }
-    }
-    async getCurrentUser(){
-        try {
-            return await this.account.createEmailPasswordSession(email,password);
-            
-        } catch (error) {
-          console.log(error)
-          toast.error('Couldnot get user')   
-        }
-        return null;
     }
 
-    async logout(){
+    async login({ email, password }: User) {
+        try {
+            const session = await this.account.createEmailSession(email, password);
+            toast.success("Login successful");
+            return session;
+        } catch (error) {
+            console.log("Login error:", error);
+            toast.error("Couldn't login now. Try Again Later");
+            throw error;
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Get current user error:", error);
+            toast.error("Could not get user");
+            return null;
+        }
+    }
+
+    async logout() {
         try {
             await this.account.deleteSessions();
-            toast.success("Logout successful")
+            toast.success("Logout successful");
         } catch (error) {
-            console.log(error)
-            toast.error('Unable to Logout')
+            console.log("Logout error:", error);
+            toast.error("Unable to Logout");
+            throw error;
         }
     }
-
-const authService = new AuthService()
-
-export default authService;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
+
+// Create a single instance and export it
+const authService = new AuthService();
+
+// Export the type and the instance
+export type { AuthService };
+export default authService;
