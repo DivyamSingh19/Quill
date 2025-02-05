464203d8796dccb35f6dfca90da499fa0b186aa9
Divyam Singh <divyamsingh1918@gmail.com>
2025-02-05 22:35:26 +0530

    Update assets.ts

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -1,8 +1,16 @@
-import logo from './logo.png'
+import logo from './logo.svg'
 import banner from './Banner.svg'
-
+import profile_icon from './profile_icon.svg'
+import bell from './bell.svg'
+import menu from './menu_icon.svg'
+import dropdown from'./dropdown_icon.png'
 export const assets={
     logo,
-    banner
+    banner,
+    profile_icon,
+    bell,
+    menu,
+    dropdown
+
 
 }
