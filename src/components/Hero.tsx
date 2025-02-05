1833a724f7f3c9a860810087e9d439411e70d9eb
Divyam Singh <divyamsingh1918@gmail.com>
2025-02-05 22:36:00 +0530

    Create Hero.tsx

---
+++ src/components/Hero.tsx
@@ -0,0 +1,42 @@
+ 
+import { useScroll, useTransform } from "framer-motion";
+import React from "react";
+import {  Landing  } from "./Landing";
+import GridLayout from "./Grid";
+import Navbar from "./Navbar";
+
+export function Hero() {
+  const ref = React.useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+
+  const pathLengthFirst = useTransform(scrollYProgress, [0, 0.8], [0.2, 1.2]);
+  const pathLengthSecond = useTransform(scrollYProgress, [0, 0.8], [0.15, 1.2]);
+  const pathLengthThird = useTransform(scrollYProgress, [0, 0.8], [0.1, 1.2]);
+  const pathLengthFourth = useTransform(scrollYProgress, [0, 0.8], [0.05, 1.2]);
+  const pathLengthFifth = useTransform(scrollYProgress, [0, 0.8], [0, 1.2]);
+
+  return (
+    <div>
+     <div>  <Navbar/> </div>
+    <div
+      className=" w-screen h-screen absolute top-40 left-0 pt-20 "
+      ref={ref}
+    >
+      <Landing
+        pathLengths={[
+          pathLengthFirst,
+          pathLengthSecond,
+          pathLengthThird,
+          pathLengthFourth,
+          pathLengthFifth,
+        ]}
+      />
+      
+    </div>
+    
+    </div>
+  );
+}
