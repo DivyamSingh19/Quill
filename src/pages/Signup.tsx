b943806b541a5ac4ad95d068dabc2c2df39bda97
Divyam Singh <divyamsingh1918@gmail.com>
2025-02-06 21:42:49 +0530

    Update Signup.tsx

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,35 +1,32 @@
 import { useEffect } from "react";
-import { LockClosedIcon, EnvelopeIcon} from "@heroicons/react/24/solid";
+import { LockClosedIcon, EnvelopeIcon } from "@heroicons/react/24/solid";
 import { assets } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useForm } from "react-hook-form";
 import authService from "../appwrite/auth";
-import {login} from '../store/authSlice'
+import { login } from "../store/authSlice";
 import { FaGithub } from "react-icons/fa";
-import  {FcGoogle} from "react-icons/fc"
-
-
+import { FcGoogle } from "react-icons/fc";
 
 const Signup = () => {
-  const navigate = useNavigate()
-  const [errorp,setErrorp] = useState("")
-  const dispatch = useDispatch()
-  const {register,handleSubmit} = useForm()
+  const navigate = useNavigate();
+  const [errorp, setErrorp] = useState("");
+  const dispatch = useDispatch();
+  const { register, handleSubmit } = useForm();
 
-  const create = async(data)=>{
-    setErrorp("")
+  const create = async (data) => {
+    setErrorp("");
     try {
-      const userData = await authService.createAccount(data)
-      if(userData) dispatch(login(userData))
-      navigate("/")
+      const userData = await authService.createAccount(data);
+      if (userData) dispatch(login(userData));
+      navigate("/");
     } catch (error) {
-      console.log(error)
-      setErrorp(errorp.message)
-      
+      console.log(error);
+      setErrorp(errorp.message);
     }
-  }
+  };
 
   useEffect(() => {
     document.body.style.overflow = "hidden";
@@ -49,10 +46,12 @@ const Signup = () => {
         {/* Right side with login form */}
         <div className="w-full md:w-1/2 p-8 flex items-center justify-center">
           <div className="max-w-sm mx-auto">
-            <h2 className="text-white text-2xl font-medium mb-6">Welcome to Quill!</h2>
-            
+            <h2 className="text-white text-2xl font-medium mb-6">
+              Welcome to Quill!
+            </h2>
+
             <form onSubmit={handleSubmit(create)} className="space-y-4">
-            <div>
+              <div>
                 <label className="text-sm text-gray-400">Name</label>
                 <div className="relative mt-1">
                   <input
@@ -61,7 +60,7 @@ const Signup = () => {
                     placeholder="Enter your name"
                     {...register("name", {
                       required: true,
-                  })}
+                    })}
                   />
                   <EnvelopeIcon className="absolute right-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-500" />
                 </div>
@@ -76,10 +75,12 @@ const Signup = () => {
                     {...register("email", {
                       required: true,
                       validate: {
-                          matchPatern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
-                          "Email address must be a valid address",
-                      }
-                  })}
+                        matchPatern: (value) =>
+                          /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(
+                            value
+                          ) || "Email address must be a valid address",
+                      },
+                    })}
                   />
                   <EnvelopeIcon className="absolute right-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-500" />
                 </div>
@@ -93,24 +94,35 @@ const Signup = () => {
                     className="w-full bg-transparent border border-gray-800 rounded p-3 text-white focus:outline-none focus:border-emerald-500"
                     placeholder="Enter your password"
                     {...register("password", {
-                      required: true,})}
+                      required: true,
+                    })}
                   />
-                 
+
                   <LockClosedIcon className="absolute right-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-500" />
                 </div>
               </div>
 
-              <div className="flex items-center justify-between">
+              <div className="flex items-center justify-between gap-5">
                 <label className="flex items-center justify-between">
-                  <input type="checkbox" className="rounded border-gray-800 text-emerald-500 focus:ring-emerald-500" />
-                  <span className="ml-2 text-sm text-gray-400">Remember me{" "}</span>
+                  <input
+                    type="checkbox"
+                    className="rounded border-gray-800 text-emerald-500 focus:ring-emerald-500"
+                  />
+                  <span className="ml-2 text-sm text-gray-400">
+                    Remember me{" "}
+                  </span>
                 </label>
-                <a href="/reset-password" className="text-sm text-emerald-500 hover:text-emerald-400">Forgot Password?</a>
+                <a
+                  href="/reset-password"
+                  className="text-sm text-emerald-500 hover:text-emerald-400"
+                >
+                  Forgot Password?
+                </a>
               </div>
 
               <button
                 type="submit"
-                className="w-full bg-emerald-500 text-white rounded p-3 hover:bg-emerald-600 transition-colors"
+                className="w-full bg-green-700 text-white rounded p-3 hover:bg-emerald-600 transition-colors"
               >
                 Signup
               </button>
@@ -123,23 +135,26 @@ const Signup = () => {
 
               <button
                 type="button"
-                className="w-full border border-gray-800 text-white rounded p-3 hover:bg-gray-900 transition-colors"
+                className="w-full border border-gray-800 text-white rounded p-3 hover:bg-gray-900 transition-colors flex items-center justify-center gap-6"
               >
-               <FcGoogle size={24}/>
+                <FcGoogle size={20} />
                 Signup with Google
               </button>
               <button
                 type="button"
-                className="w-full border border-gray-800 text-white rounded p-3 hover:bg-gray-900 transition-colors"
+                className="w-full border border-gray-800 text-white rounded p-3 hover:bg-gray-900 transition-colors flex items-center justify-center gap-6"
               >
-                <FaGithub size={24}/>
+                <FaGithub size={20} />
                 Signup with Github
               </button>
             </form>
 
             <p className="mt-6 text-center text-sm text-gray-400">
               Already on Quill?{" "}
-              <a href="/login" className="text-emerald-500 hover:text-emerald-400">
+              <a
+                href="/login"
+                className="text-emerald-500 hover:text-emerald-400"
+              >
                 Login now
               </a>
             </p>
