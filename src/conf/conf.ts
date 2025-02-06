8e99b3b70412ebd378cdd2d7af1987e09736c913
Divyam Singh <divyamsingh1918@gmail.com>
2025-02-06 21:42:38 +0530

    Updates

--- src/conf/conf.ts
+++ src/conf/conf.ts
@@ -4,6 +4,8 @@ const conf = {
     appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+    googleAuthSuccessUrl: String(import.meta.env.VITE_GOOGLE_AUTH_SUCCESS_URL),
+    googleAuthFailureUrl: String(import.meta.env.VITE_GOOGLE_AUTH_FAILURE_URL)
 }
 
 
--- src/pages/AddPost.tsx
+++ src/pages/AddPost.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import { DotBackgroundDemo } from '../components/DotBg'
+ 
+import { Card } from '../components/Card'
 
 const AddPost = () => {
   return (
     <div>
-      <DotBackgroundDemo/>
+      <Card text={"New Card"} image={undefined} description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quis animi sapiente. Fugiat nulla recusandae expedita dolorem provident at tempora asperiores. Optio facere rem commodi dolores quod ipsam beatae amet."} />
+       
     </div>
   )
 }
