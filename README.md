efc49b625a4572f3604e01c13e389a6a219e3b3c
Divyam Singh <divyamsingh1918@gmail.com>
2025-02-09 02:10:42 +0530

    Updates

--- README.md
+++ README.md
@@ -1,10 +1,3 @@
 UNDER DEVELOPMENT 🧑‍💻🧑‍💻🧑‍💻
 
-
-
-
-
-
-
-
 Come back on 09|02|25
--- src/conf/conf.ts
+++ src/conf/conf.ts
@@ -2,8 +2,10 @@ const conf = {
     appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
     appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
     appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_BLOGPOST_ID),
+    appwriteCollectionAvatarId: String(import.meta.env.VITE_APPWRITE_AVATAR_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_IMAGES_ID),
+    appwriteBucketAvatarId: String(import.meta.env.VITE_APPWRITE_BUCKET_AVATAR_ID),
     googleAuthSuccessUrl: String(import.meta.env.VITE_GOOGLE_AUTH_SUCCESS_URL),
     googleAuthFailureUrl: String(import.meta.env.VITE_GOOGLE_AUTH_FAILURE_URL),
     rteAPI:String(import.meta.env.RTE_API)
