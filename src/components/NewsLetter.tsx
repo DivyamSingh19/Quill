1b530b04375b3c649192d521b448daf0debe0728
Divyam Singh <divyamsingh1918@gmail.com>
2025-02-10 01:02:23 +0530

    Update NewsLetter.tsx

--- src/components/NewsLetter.tsx
+++ src/components/NewsLetter.tsx
@@ -9,7 +9,7 @@ const NewsletterBox = () => {
         toast.dark("Thank you for registering")
     }
     return (
-     <div className=" top-500 md:top-520  left-0 relative "> 
+     <div className=" top-500 md:top-390  left-0 relative "> 
       <div className="text-center">
         <p className="text-2xl font-medium text-gray-200">Subscribe now to get latest updates</p>
         <p className="text-gray-400 mt-3">
@@ -17,7 +17,7 @@ const NewsletterBox = () => {
         </p>
         <form onSubmit={onSubmitHandler} className="w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3">
           <input className="w-full sm:flex-1 outline-none"  type="email" placeholder="Enter your email" required/>
-          <button  type ='submit' className="bg-black text-white text-xs px-10 py-4 cursor pointer">SUBSCRIBE</button>
+          <button  type ='submit' className="bg-black text-white text-xs px-10 py-4 cursor pointer border border-white">SUBSCRIBE</button>
         </form>
          
       </div>
