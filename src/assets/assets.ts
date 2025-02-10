4aafef114be11090caa1b6516e4205e94c9def50
Divyam Singh <divyamsingh1918@gmail.com>
2025-02-11 02:08:49 +0530

    Update assets.ts

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -5,6 +5,9 @@ import bell from './bell.svg'
 import menu from './menu_icon.svg'
 import dropdown from'./dropdown_icon.png'
 import theme1 from './themeimg1.svg'
+import science from'./scintech.svg'
+import business from'./business.svg'
+import pd from './PersonalDevelopment.svg'
 export const assets={
     logo,
     banner,
@@ -12,7 +15,10 @@ export const assets={
     bell,
     menu,
     dropdown,
-    theme1
+    theme1,
+    science,
+    business,
+    pd
 
 
 }
